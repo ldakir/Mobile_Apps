b0bdac0a7d8c72b28517be515e29dc3254e4998e
Lamiaa Dakir <39743074+ldakir@users.noreply.github.com>
2019-08-08 20:30:26 -0400

    App that converts Kmto Miles and vice versa + simulation

---
+++ ConvertMilesToKm/app/src/main/java/com/example/convertmilestokm/MainActivity.java
@@ -0,0 +1,49 @@
+package com.example.convertmilestokm;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+
+import java.text.DecimalFormat;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        Button buttonConvMilesToKm = (Button) findViewById(R.id.buttonConvMilesToKm);
+        buttonConvMilesToKm.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                EditText textBoxMiles = (EditText) findViewById(R.id.editTextMiles);
+                EditText textBoxKm =  (EditText) findViewById(R.id.editTextKm);
+                double vMiles = Double.valueOf(textBoxMiles.getText().toString());
+                double vKm = vMiles / 0.62137;
+                DecimalFormat formatval = new DecimalFormat("##.##");
+                textBoxKm.setText(formatval.format(vKm));
+
+
+            }
+        });
+
+        Button buttonConvKmToMiles = (Button) findViewById(R.id.buttonConvKmToMiles);
+        buttonConvKmToMiles.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                EditText textBoxMiles = (EditText) findViewById(R.id.editTextMiles);
+                EditText textBoxKm = (EditText) findViewById(R.id.editTextKm);
+                double vKm = Double.valueOf(textBoxKm.getText().toString());
+                double vMiles = vKm * 0.62137;
+                DecimalFormat formatval = new DecimalFormat("##.##");
+                textBoxMiles.setText(formatval.format(vMiles));
+
+
+            }
+        });
+
+    }
+}
